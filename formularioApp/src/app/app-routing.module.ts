8f5282b114b568e29e19a22e4720b7be31679bb6
Gonzalo Gestri <gonzalogestri92299@gmail.com>
2023-04-12 20:10:55 -0300

    carga peresoza

--- formularioApp/src/app/app-routing.module.ts
+++ formularioApp/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'template',
+    loadChildren: () => import("./reactive/reactive.module").then(m => m.ReactiveModule)
+  },
+  {
+    path:'reactive',
+    loadChildren: () => import("./template/template.module").then(m => m.TemplateModule)
+  },
+  {
+    path: '404',
+    redirectTo: '',
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
